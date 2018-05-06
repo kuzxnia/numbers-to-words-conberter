83d1605e5b5986f0bc234734f32b0c5838605f84
kuzxnia <kacper.kuzniarski@gmail.com>
2018-05-06 22:31:47 +0200

    added basic converter

---
+++ src/main/kotlin/com/kuzniarski/toWords.kt
@@ -0,0 +1,70 @@
+package com.kuzniarski
+
+var TEN = 10
+var ONE_HUNDRED = 100
+var ONE_THOUSAND = 1000
+var ONE_MILLION = 1000000
+var ONE_BILLION = 1000000000
+var ONE_TRILLION = 1000000000000
+var ONE_QUADRILLION = 1000000000000000
+var MAX = 9007199254740992
+
+var TO_TWENTY = arrayOf(
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+)
+
+var TENTHS_LESS_THAN_HUNDRED = arrayOf(
+        "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+)
+
+fun convertToWord(num: Long) : String {
+    val words = ArrayList<String>()
+    var number = num
+    
+    while(number != 0.toLong()) {
+
+        when {
+            number < 20 -> {
+                words.add(TO_TWENTY[number.toInt()])
+                number = 0
+            }
+            number < ONE_HUNDRED -> {
+                val remainder :Int= (number % TEN).toInt()
+                var word = TENTHS_LESS_THAN_HUNDRED[(number / TEN).toInt()]
+                if (remainder < 10) word += "-" + TO_TWENTY[remainder]
+
+                words.add(word)
+                number = 0
+            }
+            number < ONE_THOUSAND -> {
+                words.add( convertToWord(number / ONE_HUNDRED) + " hundred, ")
+                number %= ONE_HUNDRED
+            }
+            number < ONE_MILLION -> {
+                words.add( convertToWord(number / ONE_THOUSAND) + " thousand, ")
+                number %= ONE_THOUSAND
+            }
+            number < ONE_BILLION -> {
+                words.add( convertToWord(number / ONE_MILLION) + " million, ")
+                number %= ONE_MILLION
+            }
+            number < ONE_TRILLION -> {
+                words.add( convertToWord(number / ONE_BILLION) + " billion, ")
+                number %= ONE_BILLION
+            }
+            number < ONE_QUADRILLION -> {
+                words.add( convertToWord(number / ONE_TRILLION) + " trillion, ")
+                number %= ONE_TRILLION
+            }
+            number <= MAX -> {
+                words.add( convertToWord(number / ONE_QUADRILLION) + " quadrillion, ")
+                number %= ONE_QUADRILLION
+            }
+        }
+
+    }
+    return words.joinToString(separator = "")
+}
+
+fun main(args: Array<String>) = print(convertToWord(1204))
--- src/test/kotlin/com/kuzniarski/NumbersToWordsConverterApplicationTests.kt
+++ src/test/kotlin/com/kuzniarski/NumbersToWordsConverterApplicationTests.kt
@@ -1,16 +1,16 @@
 package com.kuzniarski
 
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.junit.runners.JUnit4
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+@RunWith(JUnit4::class)
 class NumbersToWordsConverterApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Assert.assertEquals("one hundred twenty", convertToWord(120))
 	}
 
 }
